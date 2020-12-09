e331d1edb129b85186903c89819da027b38b9b27
UtkS <63444622+UtkS@users.noreply.github.com>
2020-12-10 00:22:03 +0530

    final macros uploaded

---
+++ ptplots/ptplot6080.C
@@ -0,0 +1,39 @@
+void ptplot6080()
+{
+        TFile *f=new TFile("13TeV_CR0_RHoff.root");
+        TTree *tree = (TTree*)f->Get("pytree6080");
+        Long64_t entries = (Long64_t)tree->GetEntries();
+
+        const Long64_t maxTrack=100000;
+
+    Int_t ntrack;
+    Double_t pT[maxTrack];
+
+    tree->SetBranchAddress("ntrack",&ntrack);
+    tree->SetBranchAddress("pT",&pT);
+
+        TH1D *hpT = new TH1D("hpT","Transverse Momentum 60-80",100,0.0,5.0);
+        TCanvas *c1=new TCanvas();
+        c1->SetLogy();
+
+        for(Int_t i=1;i<entries;i++){
+
+            tree->GetEntry(i);
+            Int_t ntrk = ntrack;
+
+            for(Int_t j=0;j<ntrk;j++){
+
+                Double_t pT1 = pT[j];
+                hpT->Fill(pT1);
+            }
+        }
+	hpT->GetXaxis()->SetTitle("Transverse Momentum");
+        hpT->GetYaxis()->SetTitle("Counts");
+        hpT->Draw();
+        hpT->Fit("expo");
+        auto legend = new TLegend(0.1,0.7,0.48,0.9);
+        legend->SetHeader("The Legend Title","C");
+        legend->AddEntry(hpT,"Transverse Momentum","f");
+        legend->AddEntry("expo","Exponential Fit","l");
+	legend->Draw();
+}
