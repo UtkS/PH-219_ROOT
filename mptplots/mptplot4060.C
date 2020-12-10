9157ca96401635eceaca0ca1dd6b1122f460bd31
Malavika <65826890+malavikabalamurugan@users.noreply.github.com>
2020-12-10 12:45:33 +0530

    Add files via upload

---
+++ mptplots/mptplot4060.C
@@ -0,0 +1,44 @@
+void mptplot4060()
+{
+        TFile *f=new TFile("13TeV_CR0_RHoff.root");
+        TTree *tree = (TTree*)f->Get("pytree4060");
+        Long64_t entries = (Long64_t)tree->GetEntries();
+
+        const Long64_t maxTrack=100000;
+    Int_t ntrack;
+    Double_t pT[maxTrack];
+    Double_t mpT=0.0;	    
+
+    tree->SetBranchAddress("ntrack",&ntrack);
+    tree->SetBranchAddress("pT",&pT);
+
+        TH1D *hpT = new TH1D("hpT","Average Transverse Momentum 40-60",100,0.0,5.0);
+        TCanvas *c1=new TCanvas();
+        c1->SetLogy();
+
+        for(Int_t i=0;i<entries;i++){
+
+            tree->GetEntry(i);
+            Int_t ntrk = ntrack;
+
+            for(Int_t j=0;j<ntrk;j++){
+
+                mpT += pT[j];
+                 }
+        if(ntrack!=0)
+	mpT=mpT/ntrack;
+        hpT->Fill(mpT);
+	}
+    
+	hpT->GetXaxis()->SetTitle("<pT> (GeV/c)");
+	hpT->GetYaxis()->SetTitle("Counts");
+
+        hpT->Draw();
+        hpT->Fit("gaus");
+        auto legend = new TLegend(0.1,0.7,0.36,0.9);
+        legend->SetHeader("Legend","C");
+        legend->AddEntry(hpT,"Average Transverse Momentum","f");
+        legend->AddEntry(gaus,"Gaussian Fit","l");
+        legend->Draw();
+
+}
